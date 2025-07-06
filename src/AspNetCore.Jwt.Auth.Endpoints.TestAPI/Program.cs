bac4396364d9b61ce50b8ba4fc64fe28f318fa93
Damien <damientohin@gmail.com>
2025-07-06 18:43:49 +0200

    Added swagger

--- src/AspNetCore.Jwt.Auth.Endpoints.TestAPI/Program.cs
+++ src/AspNetCore.Jwt.Auth.Endpoints.TestAPI/Program.cs
@@ -12,8 +12,9 @@ using AspNetCore.Jwt.Auth.Endpoints.Helpers;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=app.db"));
@@ -72,7 +73,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestAPI v1");
+        c.RoutePrefix = string.Empty; // Launch Swagger UI at the app's root
+    });
 }
 
 app.UseHttpsRedirection();
